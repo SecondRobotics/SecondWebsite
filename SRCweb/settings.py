86d26193a7d1ec27820696ff39605a8d97971d98
Brennan Bibic <brennan.bibic@gmail.com>
2021-01-15 00:47:45 -0500

    Update staticfiles location + gitignore for secrets

--- SRCweb/settings.py
+++ SRCweb/settings.py
@@ -133,9 +133,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
