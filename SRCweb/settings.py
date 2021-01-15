daf1257750e6f9f46a4bb3d93bdc51eccd109170
Brennan Bibic <brennan.bibic@gmail.com>
2021-01-15 11:32:40 -0500

    Debug mode off, google translate fix

--- SRCweb/settings.py
+++ SRCweb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('BASE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["secondrobotics.org", "www.secondrobotics.org", "localhost"]
 
