97eb32fe578c6899a6795da789eaac5dfe182711
Nicholas Bottone <nick@bottone.io>
2021-01-31 20:20:04 -0500

    Remove print statements from views

--- events/views.py
+++ events/views.py
@@ -8,14 +8,12 @@ def event_summary(response):
     context = []
     for event in events:
         players = Player.objects.filter(event__name=event.name)
-        print(len(players))
         context.append({"player_num": len(players), "event": event})
 
     return render(response, "events/event_summary.html", {"context": context})
 
 def robot_event(response, event_name, tab):
     players = Player.objects.filter(event__name=event_name).order_by("player_name")
-    print(players)
     num = []
     i = 1 
     for player in players:
