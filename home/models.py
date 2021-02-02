878a3e82c053daf079aea2b6e8c93030307c2c83
Brennan Bibic <brennan.bibic@gmail.com>
2021-02-02 09:05:34 -0500

    Profile model name showing

--- home/models.py
+++ home/models.py
@@ -6,3 +6,5 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     profile_picture = models.URLField(default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI7M4Z0v1HP2Z9tZmfQaZFCuspezuoxter_A&usqp=CAU")
+    def __str__(self):
+        return self.user
