164834ed95fcfdd69d961b09779114656341ebbf
Nicholas Bottone <nick@bottone.io>
2022-04-14 00:29:45 -0400

    Bump minimum version to v7.1f

--- highscores/views.py
+++ highscores/views.py
@@ -532,7 +532,8 @@ def check_generic_game_settings(score_obj: Score, request):
     :return: None if the settings are valid, or a response with an error message if they are not.
     """
     if float(score_obj.client_version[1:4]) < 7.1 or score_obj.client_version == 'v7.1a' or \
-            score_obj.client_version == 'v7.1b' or score_obj.client_version == 'v7.1c':
+            score_obj.client_version == 'v7.1b' or score_obj.client_version == 'v7.1c' or \
+            score_obj.client_version == 'v7.1d' or score_obj.client_version == 'v7.1e':
         return error_response(request, WRONG_VERSION_MESSAGE)
     if "pre" in score_obj.client_version:
         return error_response(request, PRERELEASE_MESSAGE)
