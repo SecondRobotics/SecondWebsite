b4c793d5cf727816360c84001123a7e19488fcbb
Nicholas Bottone <nick@bottone.io>
2022-07-07 21:38:15 -0400

    Design models for ranked

--- ranked/models.py
+++ ranked/models.py
@@ -2,3 +2,58 @@ from django.db import models
 from discordoauth2.models import User
 
 # Create your models here.
+
+
+class GameMode(models.Model):
+    name = models.CharField(max_length=25)
+    game = models.CharField(max_length=25)
+    players_per_alliance = models.IntegerField(min=1, max=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    match_number = models.AutoField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)
+    game_mode = models.ForeignKey(GameMode, on_delete=models.CASCADE)
+
+    red_alliance = models.ManyToManyField(User, related_name="red_alliance")
+    blue_alliance = models.ManyToManyField(User, related_name="blue_alliance")
+
+    red_score = models.IntegerField()
+    blue_score = models.IntegerField()
+
+    red_starting_elo = models.IntegerField()
+    blue_starting_elo = models.IntegerField()
+
+    red_players_starting_elo = models.CommaSeparatedIntegerField()
+    blue_players_starting_elo = models.CommaSeparatedIntegerField()
+
+    def get_red_players(self):
+        return self.red_alliance.all()
+
+    def get_blue_players(self):
+        return self.blue_alliance.all()
+
+    def __str__(self):
+        return f"{self.match_number} - {self.game_mode} - {self.time}"
+
+
+class PlayerElo(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    game_mode = models.ForeignKey(GameMode, on_delete=models.CASCADE)
+
+    elo = models.IntegerField(default=1000)
+
+    matches_played = models.IntegerField(default=0)
+    matches_won = models.IntegerField(default=0)
+    matches_lost = models.IntegerField(default=0)
+    matches_drawn = models.IntegerField(default=0)
+    last_match_played = models.DateTimeField(null=True, blank=True)
+    total_score = models.IntegerField(default=0)
+
+    elo_history = models.CommaSeparatedIntegerField(default="")
+
+    def __str__(self):
+        return f"{self.player} - {self.game_mode} - {self.elo}"
