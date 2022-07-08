524732b10acecd72b3a7905065812612fdc2fb6f
Nicholas Bottone <nick@bottone.io>
2022-07-07 22:04:20 -0400

    Add more user fields to admin view

--- discordoauth2/admin.py
+++ discordoauth2/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import User
-from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('password', 'first_name', 'last_name')
+
+    list_display = ('username', 'display_name', 'email', 'is_staff', 'is_active',
+                    'is_superuser', 'last_login', 'date_joined',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active',
+                   'last_login', 'date_joined',)
+    search_fields = ('username', 'display_name', 'email',)
+
 
 admin.site.register(User, UserAdmin)
