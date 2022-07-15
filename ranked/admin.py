8073ee51df58b8b1f8b6ceb3de1678f3e74c19c1
Nicholas Bottone <nick@bottone.io>
2022-07-15 10:27:25 -0400

    Fix ranked admin panel displays

--- ranked/admin.py
+++ ranked/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import GameMode, Match, PlayerElo
+from .models import GameMode, Match, PlayerElo, EloHistory
 
 # Register your models here.
 
@@ -8,12 +8,11 @@ from .models import GameMode, Match, PlayerElo
 class GameModeAdmin(admin.ModelAdmin):
     list_display = ('name', 'game', 'players_per_alliance',)
     list_filter = ('name', 'game', 'players_per_alliance',)
-    search_fields = ('name')
+    search_fields = ('name',)
 
 
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('match_number', 'time', 'game_mode',
-                    'red_alliance', 'blue_alliance', 'red_score', 'blue_score')
+    list_display = ('match_number', 'time', 'game_mode', 'red_score', 'blue_score')
     list_filter = ('match_number', 'time', 'game_mode')
     search_fields = ('match_number', 'time')
 
@@ -21,11 +20,17 @@ class MatchAdmin(admin.ModelAdmin):
 class PlayerEloAdmin(admin.ModelAdmin):
     list_display = ('player', 'game_mode', 'elo',
                     'matches_played', 'matches_won', 'matches_lost', 'matches_drawn')
-    list_filter = ('game_mode')
-    search_fields = ('player')
+    list_filter = ('game_mode',)
+    search_fields = ('player',)
+
+class EloHistoryAdmin(admin.ModelAdmin):
+    list_display = ('player_elo', 'match_number', 'elo')
+    list_filter = ('player_elo', 'match_number')
+    search_fields = ('player_elo',)
 
 
 admin.site.site_header = "Second Robotics Admin Panel"
 admin.site.register(GameMode, GameModeAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(PlayerElo, PlayerEloAdmin)
+admin.site.register(EloHistory, EloHistoryAdmin)
