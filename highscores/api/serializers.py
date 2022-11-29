69a2af815312dd2634bdf4647c345d8e44dfe1f1
Nicholas Bottone <nick@bottone.io>
2022-11-29 18:37:10 -0500

    Implement api endpoints to get scores

--- highscores/api/serializers.py
+++ highscores/api/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from discordoauth2.models import User
+from ..models import Score, Leaderboard
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+# ScoreSerializer contains the fields leaderboard, player (display name and avatar), and score.
+class ScoreSerializer(ModelSerializer):
+    class Meta:
+        model = Score
+        fields = ['leaderboard', 'player', 'score']
+
+
+class LeaderboardSerializer(ModelSerializer):
+    class Meta:
+        model = Leaderboard
         fields = '__all__'
