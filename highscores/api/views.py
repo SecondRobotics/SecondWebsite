2f1635f6de83cf7b6a69fd5fc928ae52e23274db
Nicholas Bottone <nick@bottone.io>
2022-11-29 17:09:02 -0500

    Implement highscore submission api

--- highscores/api/views.py
+++ highscores/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect
-from django.utils import timezone
+from django.http import HttpResponse, HttpRequest
 from rest_framework.response import Response
-from rest_framework.request import Request
+from rest_framework.request import Request, Empty
 from rest_framework.decorators import api_view
-from discordoauth2.models import User
-from highscores.api.serializers import UserSerializer
+
+from .serializers import UserSerializer
+from ..models import Score, Leaderboard
 
 
 @api_view(['GET'])
@@ -22,7 +23,7 @@ def get_session_validity(request: Request) -> Response:
 
 
 @api_view(['GET'])
-def auth(request: Request) -> Response:
+def auth(request: HttpRequest) -> HttpResponse:
     """Authenticates the user."""
     if request.user.is_authenticated:
         # Redirect to localhost:22226 with the sessionid as a query parameter.
@@ -37,4 +38,47 @@ def submit(request: Request) -> Response:
     if not request.user.is_authenticated:
         return Response({'success': False, 'message': 'User is not authenticated.'})
 
-    return Response({'success': False, 'message': 'Not implemented yet.'})
+    if not request.data or request.data is Empty:
+        return Response({'success': False, 'message': 'No data was provided.'})
+    data = request.data
+
+    # Get the score from the request.
+    score = getattr(data, 'score', None)  # type: int | None
+    leaderboard = getattr(data, 'leaderboard', None)  # type: str | None
+    source = getattr(data, 'source_link', None)  # type: str | None
+    clean_code = getattr(data, 'clean_code', None)  # type: str | None
+
+    if score is None or leaderboard is None or source is None or clean_code is None:
+        return Response({'success': False, 'message': 'Missing data.'})
+
+    leaderboard_obj = Leaderboard.objects.get(name=leaderboard)
+    if leaderboard_obj is None:
+        return Response({'success': False, 'message': 'Invalid leaderboard.'})
+
+    # Determine the leaderboard to submit to.
+    if leaderboard == 'Infinite Recharge':
+        from ..lib import submit_infinite_recharge as submit_score
+    elif leaderboard == 'Rapid React':
+        from ..lib import submit_rapid_react as submit_score
+    elif leaderboard == 'Freight Frenzy':
+        from ..lib import submit_freight_frenzy as submit_score
+    elif leaderboard == 'Tipping Point':
+        from ..lib import submit_tipping_point as submit_score
+    elif leaderboard == 'Spin Up':
+        from ..lib import submit_spin_up as submit_score
+    else:
+        return Response({'success': False, 'message': 'Invalid leaderboard.'})
+
+    score_obj = Score()
+    score_obj.leaderboard = leaderboard_obj
+    score_obj.user = request.user  # type: ignore
+    score_obj.score = score
+    score_obj.source = source
+    score_obj.clean_code = clean_code
+
+    # Submit the score.
+    res = submit_score(score_obj)
+    if res is not None:
+        return Response({'success': False, 'message': res})
+
+    return Response({'success': True, 'message': 'Score submitted.'})
