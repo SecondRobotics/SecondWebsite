08b47c7efcea6f3237adc8902b4acbe30a2e49f6
Nicholas Bottone <nick@bottone.io>
2022-11-30 01:49:14 -0500

    Improve leaderboard endpoint and user serializer

--- highscores/api/serializers.py
+++ highscores/api/serializers.py
@@ -6,11 +6,14 @@ from ..models import Score, Leaderboard
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'display_name', 'username',
+                  'avatar', 'date_joined', 'is_staff']
 
 
 # ScoreSerializer contains the fields leaderboard, player (display name and avatar), and score.
 class ScoreSerializer(ModelSerializer):
+    player = UserSerializer()
+
     class Meta:
         model = Score
         fields = ['leaderboard', 'player', 'score']
