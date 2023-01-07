ecf5f2a69bbc552d38a40dcf729a2c6f57536f3e
Nicholas Bottone <nick@bottone.io>
2023-01-07 14:06:57 -0500

    Remove manual highscore submit button

--- highscores/urls.py
+++ highscores/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("<str:game_slug>/submit/", views.submit_form, name="score submit"),
+    #     path("<str:game_slug>/submit/", views.submit_form, name="score submit"),
     path("<str:game_slug>/combined/", views.leaderboard_combined,
          name="game leaderboard"),
     path("<str:game_slug>/<str:name>/",
