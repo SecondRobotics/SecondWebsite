07a27ee4c71c6c715bf0ad85211781618d800370
Nicholas Bottone <nick@bottone.io>
2023-01-18 13:00:08 -0500

    Fix client version check on highscores

--- highscores/lib.py
+++ highscores/lib.py
@@ -263,8 +263,7 @@ def check_generic_game_settings(score_obj: Score, auto_or_teleop: str) -> Union[
     """ Checks if the universal game settings are valid.
     :return: None if the settings are valid, or a response with an error message if they are not.
     """
-    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 10.0
-            or score_obj.client_version[5] == 'a' or score_obj.client_version[5] == 'b'):
+    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 10.1):
         return WRONG_VERSION_MESSAGE
     if "_p" in score_obj.client_version:
         return PRERELEASE_MESSAGE
