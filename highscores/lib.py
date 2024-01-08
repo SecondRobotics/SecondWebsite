c223197ed8f5b872040501973da9343375ff26a7
Nicholas Bottone <nick@bottone.io>
2024-01-08 10:04:33 -0500

    Update minimum required client version to 13.1

--- highscores/lib.py
+++ highscores/lib.py
@@ -325,7 +325,7 @@ def check_generic_game_settings(score_obj: Score, auto_or_teleop: str) -> Union[
     """ Checks if the universal game settings are valid.
     :return: None if the settings are valid, or a response with an error message if they are not.
     """
-    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 11.0):
+    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 13.1):
         # or score_obj.client_version == 'v10.4a' or score_obj.client_version == 'v10.4b'
         return WRONG_VERSION_MESSAGE
     if "_p" in score_obj.client_version:
