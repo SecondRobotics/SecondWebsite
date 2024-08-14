5eeab018792aed2431361b7e7f0ab814f0636634
Brennan Bibic <brennan.bibic@gmail.com>
2024-08-14 15:47:51 -0400

    Fix email spam, maybe do color formatting

--- ranked/templatetags/rank_filter.py
+++ ranked/templatetags/rank_filter.py
@@ -5,29 +5,29 @@ register = template.Library()
 @register.simple_tag
 def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
     if highest_mmr == lowest_mmr:
-        return 'Stone'  # All players have the same MMR, edge case
+        return 'Stone', '#ffffff'  # All players have the same MMR, edge case
 
     # Calculate the percentile
     percentile = (highest_mmr - mmr) / (highest_mmr - lowest_mmr)
     
     # Determine rank based on percentile
     if percentile <= 0.1:
-        return 'Challenger'
+        return 'Challenger', '#ffffff'
     elif percentile <= 0.2:
-        return 'Grandmaster'
+        return 'Grandmaster', '#ffffff'
     elif percentile <= 0.3:
-        return 'Master'
+        return 'Master', '#ffffff'
     elif percentile <= 0.4:
-        return 'Diamond'
+        return 'Diamond', '#ffffff'
     elif percentile <= 0.5:
-        return 'Platinum'
+        return 'Platinum', '#ffffff'
     elif percentile <= 0.6:
-        return 'Gold'
+        return 'Gold', '#ebce75'
     elif percentile <= 0.7:
-        return 'Silver'
+        return 'Silver', '#ffffff'
     elif percentile <= 0.8:
-        return 'Bronze'
+        return 'Bronze', '#ffffff'
     elif percentile <= 0.9:
-        return 'Iron'
+        return 'Iron', '#ffffff'
     else:
-        return 'Stone'
+        return 'Stone', '#ffffff'
