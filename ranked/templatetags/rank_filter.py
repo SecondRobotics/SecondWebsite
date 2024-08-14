22dc85e8617143b9a5a475195fe8467bd8e90b6b
Brennan Bibic <brennan.bibic@gmail.com>
2024-08-14 16:40:12 -0400

    Fill out title colors

--- ranked/templatetags/rank_filter.py
+++ ranked/templatetags/rank_filter.py
@@ -12,22 +12,22 @@ def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
 
     # Determine rank based on percentile
     if percentile <= 0.1:
-        return 'Challenger', '#ffffff'
+        return 'Challenger', '#c7ffff'
     elif percentile <= 0.2:
-        return 'Grandmaster', '#ffffff'
+        return 'Grandmaster', '#eb8686'
     elif percentile <= 0.3:
-        return 'Master', '#ffffff'
+        return 'Master', '#f985cb'
     elif percentile <= 0.4:
-        return 'Diamond', '#ffffff'
+        return 'Diamond', '#c6d2ff'
     elif percentile <= 0.5:
-        return 'Platinum', '#ffffff'
+        return 'Platinum', '#54eac1'
     elif percentile <= 0.6:
         return 'Gold', '#ebce75'
     elif percentile <= 0.7:
-        return 'Silver', '#ffffff'
+        return 'Silver', '#d9d9d9'
     elif percentile <= 0.8:
-        return 'Bronze', '#ffffff'
+        return 'Bronze', '#b8a25e'
     elif percentile <= 0.9:
         return 'Iron', '#ffffff'
     else:
-        return 'Stone', '#ffffff'
+        return 'Stone', '#000000'
