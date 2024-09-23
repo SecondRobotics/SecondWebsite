39928a8b5742ddfd23f5ea7f315cdf0f6d50abce
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-23 14:49:36 -0400

    Color tuning for toles

--- ranked/templatetags/rank_filter.py
+++ ranked/templatetags/rank_filter.py
@@ -13,7 +13,7 @@ def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
     # Determine rank based on percentile
 
     if percentile <= 0.08:
-        return 'Untouchable', '#7a00ff'
+        return 'Untouchable', '#973df6'
     elif percentile <= 0.16:
         return 'Legend', '#cb001c'
     elif percentile <= 0.25:
@@ -21,7 +21,7 @@ def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
     elif percentile <= 0.33:
         return 'Master', '#f985cb'
     elif percentile <= 0.41:
-        return 'Challenger', '#FF4500'
+        return 'Challenger', '#8d2d60'
     elif percentile <= 0.50:
         return 'Diamond', '#c6d2ff'
     elif percentile <= 0.58:
