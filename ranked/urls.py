28c5fe5cad081ca18533807cb991d83b3ea76492
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-23 14:34:58 -0400

    Revert "Try global leaderboard stuff"
    This reverts commit b739537c97f5cd36ea3b61f1b41c5a186764ad9d.

--- ranked/urls.py
+++ ranked/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.ranked_home, name='home'),
     path('<str:name>/', views.leaderboard, name='leaderboard'),
     path('<str:name>/<str:player_id>', views.player_info, name='player_info'),
-    path('global_leaderboard/', views.global_leaderboard, name='global_leaderboard'),
 ]
