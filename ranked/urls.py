e409f7244dc2bb159a70f91a89b098475f31b391
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-23 14:34:54 -0400

    Revert "url ordering"
    This reverts commit 6edc52b47f9b0167dbb91a9100671335d57c7c1d.

--- ranked/urls.py
+++ ranked/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ranked_home, name='home'),
-    path('global_leaderboard/', views.global_leaderboard, name='global_leaderboard'),
     path('<str:name>/', views.leaderboard, name='leaderboard'),
     path('<str:name>/<str:player_id>', views.player_info, name='player_info'),
-    
+    path('global_leaderboard/', views.global_leaderboard, name='global_leaderboard'),
 ]
