3750f7e6b127adb26128a57917f1c783d96f9dfb
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-23 14:57:39 -0400

    color tuning

--- ranked/templatetags/rank_filter.py
+++ ranked/templatetags/rank_filter.py
@@ -15,17 +15,17 @@ def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
     if percentile <= 0.08:
         return 'Untouchable', '#973df6'
     elif percentile <= 0.16:
-        return 'Legend', '#cb001c'
+        return 'Legend', '#be89f6'
     elif percentile <= 0.25:
-        return 'Grandmaster', '#d15858'
+        return 'Grandmaster', '#cb001c'
     elif percentile <= 0.33:
-        return 'Master', '#f985cb'
+        return 'Master', '#d15858'
     elif percentile <= 0.41:
-        return 'Challenger', '#8d2d60'
+        return 'Challenger', '#f985cb'
     elif percentile <= 0.50:
         return 'Diamond', '#c6d2ff'
     elif percentile <= 0.58:
-        return 'Platinum', '#2a6873'
+        return 'Platinum', '#0089a1'
     elif percentile <= 0.66:
         return 'Gold', '#ebce75'
     elif percentile <= 0.75:
