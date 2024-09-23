636430f5d69c9ab3f605c9f91ac22ef58fd24448
Brennan Bibic <brennan.bibic@gmail.com>
2024-09-23 14:44:26 -0400

    updated ranks

--- ranked/templatetags/rank_filter.py
+++ ranked/templatetags/rank_filter.py
@@ -11,23 +11,28 @@ def mmr_to_rank(mmr, highest_mmr, lowest_mmr):
     percentile = (highest_mmr - mmr) / (highest_mmr - lowest_mmr)
 
     # Determine rank based on percentile
-    if percentile <= 0.1:
-        return 'Challenger', '#c7ffff'
-    elif percentile <= 0.2:
-        return 'Grandmaster', '#eb8686'
-    elif percentile <= 0.3:
+
+    if percentile <= 0.08:
+        return 'Untouchable', '#7a00ff'
+    elif percentile <= 0.16:
+        return 'Legend', '#cb001c'
+    elif percentile <= 0.25:
+        return 'Grandmaster', '#d15858'
+    elif percentile <= 0.33:
         return 'Master', '#f985cb'
-    elif percentile <= 0.4:
+    elif percentile <= 0.41:
+        return 'Challenger', '#FF4500'
+    elif percentile <= 0.50:
         return 'Diamond', '#c6d2ff'
-    elif percentile <= 0.5:
-        return 'Platinum', '#54eac1'
-    elif percentile <= 0.6:
+    elif percentile <= 0.58:
+        return 'Platinum', '#2a6873'
+    elif percentile <= 0.66:
         return 'Gold', '#ebce75'
-    elif percentile <= 0.7:
+    elif percentile <= 0.75:
         return 'Silver', '#d9d9d9'
-    elif percentile <= 0.8:
+    elif percentile <= 0.83:
         return 'Bronze', '#b8a25e'
-    elif percentile <= 0.9:
+    elif percentile <= 0.91:
         return 'Iron', '#ffffff'
     else:
         return 'Stone', '#000000'
