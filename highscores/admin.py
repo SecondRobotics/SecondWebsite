c367304e38a63d2a9d8b4b8faa6bea1362ab3064
Brennan Bibic <brennan.bibic@gmail.com>
2025-05-30 10:12:48 -0400

    Imporve admin interfaces

--- highscores/admin.py
+++ highscores/admin.py
@@ -1,24 +1,73 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import CleanCodeSubmission, Leaderboard, Score, ExemptedIP
 
 # Register your models here.
 
 
+@admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin):
-    list_display = ('player', 'score', 'leaderboard', 'approved',)
-    list_filter = ('approved', 'time_set', 'leaderboard')
-    search_fields = ('player__username', 'leaderboard__name')
+    list_display = ('player', 'score', 'leaderboard', 'approved', 'time_set', 'ip', 'client_version')
+    list_filter = ('approved', 'time_set', 'leaderboard', 'client_version')
+    search_fields = ('player__username', 'leaderboard__name', 'ip', 'clean_code')
     raw_id_fields = ('player', 'leaderboard')
+    readonly_fields = ('time_set', 'time_data')
+    fieldsets = (
+        ('Score Information', {
+            'fields': ('player', 'score', 'leaderboard', 'approved')
+        }),
+        ('Technical Details', {
+            'fields': ('clean_code', 'decrypted_code', 'client_version', 'time_of_score', 'robot_position', 'time_data')
+        }),
+        ('Submission Info', {
+            'fields': ('source', 'ip', 'time_set')
+        }),
+    )
+    list_per_page = 50
+    date_hierarchy = 'time_set'
+    actions = ['approve_scores', 'unapprove_scores']
+
+    def approve_scores(self, request, queryset):
+        queryset.update(approved=True)
+    approve_scores.short_description = "Approve selected scores"
+
+    def unapprove_scores(self, request, queryset):
+        queryset.update(approved=False)
+    unapprove_scores.short_description = "Unapprove selected scores"
 
 
+@admin.register(Leaderboard)
 class LeaderboardAdmin(admin.ModelAdmin):
-    list_display = ('name', 'game', 'game_slug', 'auto_or_teleop',)
-    list_filter = ('game', 'game', 'auto_or_teleop',)
-    search_fields = ('name',)
+    list_display = ('name', 'game', 'game_slug', 'auto_or_teleop', 'robot', 'message')
+    list_filter = ('game', 'auto_or_teleop')
+    search_fields = ('name', 'robot', 'message')
+    list_editable = ('message',)
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'robot', 'game', 'game_slug', 'auto_or_teleop')
+        }),
+        ('Additional Info', {
+            'fields': ('message',)
+        }),
+    )
+
+
+@admin.register(CleanCodeSubmission)
+class CleanCodeSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('player', 'score', 'leaderboard', 'time_set', 'ip')
+    list_filter = ('time_set', 'leaderboard')
+    search_fields = ('player__username', 'leaderboard__name', 'clean_code', 'ip')
+    raw_id_fields = ('player', 'leaderboard')
+    readonly_fields = ('time_set',)
+    date_hierarchy = 'time_set'
+
+
+@admin.register(ExemptedIP)
+class ExemptedIPAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'reason')
+    search_fields = ('ip', 'reason')
+    list_editable = ('reason',)
 
 
 admin.site.site_header = "Second Robotics Admin Panel"
-admin.site.register(Leaderboard, LeaderboardAdmin)
-admin.site.register(Score, ScoreAdmin)
-admin.site.register(CleanCodeSubmission)
-admin.site.register(ExemptedIP)
