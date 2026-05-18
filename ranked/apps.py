ada78444c2fa97083127f6c26da2d4a424a5208a
Brennan Bibic <brennan.bibic@gmail.com>
2026-05-18 15:40:48 -0400

    fix: guard ready() cache warm against missing table during tests

--- ranked/apps.py
+++ ranked/apps.py
@@ -6,7 +6,11 @@ class RankedConfig(AppConfig):
     name = 'ranked'
 
     def ready(self):
+        from django.db.utils import OperationalError, ProgrammingError
         from ranked.models import GameMode
         from ranked.api.lib import _margin_stats, _load_margin_stats
-        for gm in GameMode.objects.values_list("id", flat=True):
-            _margin_stats[gm] = _load_margin_stats(gm)
+        try:
+            for gm in GameMode.objects.values_list("id", flat=True):
+                _margin_stats[gm] = _load_margin_stats(gm)
+        except (OperationalError, ProgrammingError):
+            pass
