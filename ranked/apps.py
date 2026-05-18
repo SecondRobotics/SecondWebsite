d694a1e7966d31d38699a2595f6299050fe780a3
Brennan Bibic <brennan.bibic@gmail.com>
2026-05-18 15:46:42 -0400

    fix: remove ready() db query, cache warms lazily on first access instead

--- ranked/apps.py
+++ ranked/apps.py
@@ -4,13 +4,3 @@ from django.apps import AppConfig
 class RankedConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ranked'
-
-    def ready(self):
-        from django.db.utils import OperationalError, ProgrammingError
-        from ranked.models import GameMode
-        from ranked.api.lib import _margin_stats, _load_margin_stats
-        try:
-            for gm in GameMode.objects.values_list("id", flat=True):
-                _margin_stats[gm] = _load_margin_stats(gm)
-        except (OperationalError, ProgrammingError):
-            pass
