3045767a4fcc88dbbe886f228060621642d229b9
Brennan Bibic <brennan.bibic@gmail.com>
2026-05-18 16:10:25 -0400

    fix: auto-run migrate on startup to recover missing database tables

--- SRCweb/wsgi.py
+++ SRCweb/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SRCweb.settings')
 
 application = get_wsgi_application()
+
+try:
+    call_command('migrate', '--no-input', verbosity=0)
+except Exception:
+    pass
