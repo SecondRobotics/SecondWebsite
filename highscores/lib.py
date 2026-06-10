d15a37890a46b166168b365a1896e9c506c364b6
Brennan Bibic <brennan.bibic@gmail.com>
2026-06-10 12:30:29 -0400

    Bump high score client version gate to 20.0

--- highscores/lib.py
+++ highscores/lib.py
@@ -593,8 +593,8 @@ def check_generic_game_settings(score_obj: Score, auto_or_teleop: str) -> Union[
     """ Checks if the universal game settings are valid.
     :return: None if the settings are valid, or a response with an error message if they are not.
     """
-    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 19.2
-            or score_obj.client_version == 'v19.2a'):
+    if (not score_obj.client_version or float(score_obj.client_version[1:5]) < 20.0
+            or score_obj.client_version == 'v20.0a'):
         return WRONG_VERSION_MESSAGE
     if "_p" in score_obj.client_version:
         return PRERELEASE_MESSAGE
